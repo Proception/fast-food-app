ee07b7d968f043880ce651320826af2ea1da0da3
Benedict Esimaje <omasan.esimaje@gmail.com>
2018-09-20 16:33:37 +0100

    added  endpoint for users

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 import express from 'express';
 import ordersRouter from './routes/orders';
+import usersRouter from './routes/users';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/api/v1/orders', ordersRouter);
+app.use('/api/v1/users', usersRouter);
 
-
-app.listen(process.env.PORT || 3005, () => {
+app.listen(process.env.PORT || 3012, () => {
   // console.log('Example app listening on port 3000!');
 });
 
-// exports a function declared earlier
-// export { app };
-// export default class {}
 export default app;
