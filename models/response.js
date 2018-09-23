ae7bf3cbe3c34a1b8196d72b5bae798774ba3324
Benedict Esimaje <omasan.esimaje@gmail.com>
2018-09-23 13:52:26 +0100

    removed status as its being sent as a response

--- models/response.js
+++ models/response.js
@@ -1,8 +1,7 @@
-export default class Response{
-	constructor(status, code, messages, result){
-		this.status = status;
-		this.code = code;
-		this.messages = messages;
-		this.result = result;
-	}
+export default class Response {
+  constructor(status, messages, result) {
+    this.status = status;
+    this.messages = messages;
+    this.result = result;
+  }
 }
