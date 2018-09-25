7ef70d12d4e84971c8d5f124d4f86403d87fdfe0
Benedict Esimaje <omasan.esimaje@gmail.com>
2018-09-25 01:13:17 +0100

    Convert es5 classes to ES6  [Finishes #160738098]

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,104 +1,96 @@
 import uuid from 'uuid/v4';
 import Order from '../models/orders';
-import Menu from '../models/menus';
 import Response from '../models/response';
 import { jsonIsEmpty as validate } from '../utils/validate';
 // const validate = require('../utils/validate');
 
-const menu = new Menu(uuid(), 'rice', 2000, 1, 'core', new Date());
-const menu1 = new Menu(uuid(), 'beans', 500, 1, 'core', new Date());
-const menu2 = new Menu(uuid(), 'plantain', 300, 2, 'core', new Date());
-
-const menuList1 = [menu, menu2];
-const menuList2 = [menu1, menu2];
-const menuList3 = [menu, menu1, menu2];
-
-const order = new Order(uuid(), new Date(), 500000, 'new', '13, ayoade str, shomolu', menuList3, 'omasan.esimaje@gmail.com');
-const order1 = new Order(uuid(), new Date(), 700000, 'fulfilled', '14, ayoade str, shomolu', menuList2, 'oman.esimaje@gmail.com');
-const order2 = new Order(uuid(), new Date(), 600000, 'declined', '15, ayoade str, shomolu', menuList2, 'esimaje@gmail.com');
-const order3 = new Order('12245', new Date(), 900000, 'accepted', '16, ayoade str, shomolu', menuList1, 'omasan@gmail.com');
-
-
-let response;
-
-const mapOrderList = new Map([[order.orderId, order], [order1.orderId, order1],
-  [order2.orderId, order2], [order3.orderId, order3]]);
-
-// Display list of all Orders.
-function getOrderList(req, res) {
-  const status = 200;
-  response = new Response('Ok', '', mapOrderList);
-  // console.log(response, status);
-  res.status(status).send(response);
-}
-
-// Create New Order.
-function createOrder(req, res) {
-  // Get POST params
-  const json = req.body;
-  let status;
-
-
-  // Populate List in Memory if object is not empty
-  if (!(validate(json))) {
-    const newOrder = new Order(uuid(), new Date(),
-      json.orderAmount, json.orderStatus,
-      json.shippingAddress, json.menu, json.userId);
-    mapOrderList.set(newOrder.orderId, newOrder);
-    status = 201;
-    response = new Response('Ok', '', newOrder);
-    // console.log(response, status);
-  } else {
-    status = 204;
-    response = new Response('Ok', 'Unable To Create Order', json);
-    // console.log(response, status);
+export default class OrderController {
+  // Display list of all Orders.
+  constructor(response, mapOrderList) {
+    this.response = response;
+    this.mapOrderList = mapOrderList;
   }
-  res.status(status).send(response).end();
-}
 
-// Get single Order by Id
-function getOrder(req, res) {
-  const { id } = req.params;
-  const orderFound = mapOrderList.get(id);
-  // console.log('Found : ', orderFound);
-  const status = (orderFound === undefined) ? 204 : 200;
-  if (status === 200) {
-    response = new Response('Ok', '', orderFound);
+  getOrderList() {
+    const status = 200;
+    this.response = new Response('Ok', status, '', this.mapOrderList);
     // console.log(response, status);
-  } else {
-    response = new Response('Ok', 'Order does not Exist', '');
-    // console.log(response, status);
+    // res.status(status).send(response);
+    return this.response;
   }
-  res.status(status).send(response).end();
-}
 
-// Update Order by Id
-function updateOrder(req, res) {
-  const { id } = req.params;
-  // Get params in body
-  const { orderStatus } = req.body;
-  const orderFound = mapOrderList.get(id);
-  const status = (orderFound === undefined) ? 204 : 201;
-  // Set status
-  if (status === 201) {
-    orderFound.orderStatus = orderStatus;
-    mapOrderList.set(orderFound.orderId, orderFound);
-    response = new Response('Ok', '', orderFound);
-    // console.log(response, status);
-  } else {
-    response = new Response('Ok', 'Order Does not exist', '');
-    // console.log(response, status);
+  // Create New Order.
+  createOrder(req) {
+    // Get POST params
+    const json = req.body;
+    let status;
+
+
+    // Populate List in Memory if object is not empty
+    if (!(validate(json))) {
+      const newOrder = new Order(uuid(), new Date(),
+        json.orderAmount, json.orderStatus,
+        json.shippingAddress, json.menu, json.userId);
+      this.mapOrderList.set(newOrder.orderId, newOrder);
+      status = 201;
+      this.response = new Response('Ok', status, '', newOrder);
+      // console.log(response, status);
+    } else {
+      status = 204;
+      this.response = new Response('Ok', status, 'Unable To Create Order', json);
+      // console.log(response, status);
+    }
+    // res.status(status).send(response).end();
+    return this.response;
   }
-  res.status(status).send(response).end();
-}
 
-// delete Order by Id
-function deleteOrder(req, res) {
-  const { id } = req.params;
-  const status = (mapOrderList.delete(id)) ? 201 : 204;
-  res.status(status).end();
+  // Get single Order by Id
+  getOrder(req) {
+    const { id } = req.params;
+    const orderFound = this.mapOrderList.get(id);
+    // console.log('Found : ', orderFound);
+    const status = (orderFound === undefined) ? 204 : 200;
+    if (status === 200) {
+      this.response = new Response('Ok', status, '', orderFound);
+      // console.log(response, status);
+    } else {
+      this.response = new Response('Ok', status, 'Order does not Exist', '');
+      // console.log(response, status);
+    }
+    // res.status(status).send(response).end();
+    return this.response;
+  }
+
+  // Update Order by Id
+  updateOrder(req) {
+    const { id } = req.params;
+    // Get params in body
+    const { orderStatus } = req.body;
+    const orderFound = this.mapOrderList.get(id);
+    const status = (orderFound === undefined) ? 204 : 201;
+    // Set status
+    if (status === 201) {
+      orderFound.orderStatus = orderStatus;
+      this.mapOrderList.set(orderFound.orderId, orderFound);
+      this.response = new Response('Ok', status, '', orderFound);
+      // console.log(response, status);
+    } else {
+      this.response = new Response('Ok', status, 'Order Does not exist', '');
+      // console.log(response, status);
+    }
+    // res.status(status).send(response).end();
+    return this.response;
+  }
+
+  // delete Order by Id
+  deleteOrder(req) {
+    const { id } = req.params;
+    const status = (this.mapOrderList.delete(id)) ? 201 : 204;
+    // res.status(status).end();
+    return status;
+  }
 }
 // exports a function declared earlier
-export {
-  getOrderList, createOrder, getOrder, updateOrder, deleteOrder,
-};
+// export {
+//   getOrderList, createOrder, getOrder, updateOrder, deleteOrder,
+// };
--- routes/menus.js
+++ routes/menus.js
@@ -1,24 +1,54 @@
 // Require controller modules.
-import * as menuController from '../controllers/menuController';
+import uuid from 'uuid/v4';
+import MenuController from '../controllers/menuController';
+import Menu from '../models/menus';
+
 
 const express = require('express');
 
 const router = express.Router();
 
+const menu = new Menu(uuid(), 'Egusi', 500000, 4, 'addon', new Date());
+const menu1 = new Menu(uuid(), 'Fufu', 700000, 5, 'core', new Date());
+const menua = new Menu('12345', 'Yam Porridge', 900000, 12, 'Stew', new Date());
+
+let response;
+
+const mapMenuList = new Map([[menu.menuId, menu],
+  [menu1.menuId, menu1],
+  [menua.menuId, menua]]);
+const menus = new MenuController(response, mapMenuList);
 
 // GET request for returning all Menus
-router.get('/', menuController.getMenuList);
+router.get('/', (req, res) => {
+  // console.log("Without : ", menus.getMenuList(req, res));
+  const resObj = menus.getMenuList();
+  // console.log(resObj);
+  res.status(resObj.code).json(resObj);
+});
 
 // GET request for returning Menu based on id
-router.get('/:menuid', menuController.getMenu);
+router.get('/:menuid', (req, res) => {
+  // console.log("Without : ", menus.getMenu(req, res));
+  const resObj = menus.getMenu(req, res);
+  res.status(resObj.code).json(resObj);
+});
 
 // POST request for posting data
-router.post('/', menuController.createMenu);
-
+router.post('/', (req, res) => {
+  // console.log("Without : ", menus.createMenu(req, res));
+  const resObj = menus.createMenu(req, res);
+  res.status(resObj.code).json(resObj);
+});
 // PUT request for returning all Menus
-router.put('/:menuid', menuController.updateMenu);
-
+router.put('/:menuid', (req, res) => {
+  // console.log("Without : ", menus.updateMenu(req, res));
+  const resObj = menus.updateMenu(req, res);
+  res.status(resObj.code).json(resObj);
+});
 // DELETE request to Delete menu by ID
-router.delete('/:menuid', menuController.deleteMenu);
-
+router.delete('/:menuid', (req, res) => {
+  // console.log("Without : ", menus.deleteMenu(req, res));
+  res.status(menus.deleteMenu(req, res)).json();
+});
 export default router;
